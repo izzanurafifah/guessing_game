2c9034592a3f3652fa3d8128f7591eb5a4d4e5c2
izzanurafifah <izzanurafifah@gmail.com>
2025-03-07 20:10:33 +0700

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::rng().random_range(1..=100);
 
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+    
     let mut guess = String::new();
 
     io::stdin()
